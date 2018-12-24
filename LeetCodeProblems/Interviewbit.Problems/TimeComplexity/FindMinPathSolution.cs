841c4521795ca26abb23fc65bf5544606be3239b
Orest Shpak <o.shpak92@gmail.com>
2018-12-24 10:35:03 +0200

    Find Min Path

---
+++ LeetCodeProblems/Interviewbit.Problems/TimeComplexity/FindMinPathSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interviewbit.Problems.TimeComplexity
+{
+    public class FindMinPathSolution
+    {
+        private int[,] memo = new int[101, 101];
+
+        public int FindMinPath(List<List<int>> V, int r, int c)
+        {
+            CallsCount++;
+            int R = V.Count;
+            int C = V[0].Count;
+            if (r >= R || c >= C) return 100000000; // Infinity
+            if (r == R - 1 && c == C - 1) return 0;
+            if (memo[r, c] != 0) return memo[r, c];
+            CallsCount2 += 2;
+            memo[r, c] = V[r][c] + Min(FindMinPath(V, r + 1, c), FindMinPath(V, r, c + 1));
+            return memo[r, c];
+        }
+
+        private int Min(int left, int right)
+        {
+            return left <= right ? left : right;
+        }
+
+        public int CallsCount { get; set; }
+
+        public int CallsCount2 { get; set; }
+
+    }
+}
---
+++ LeetCodeProblems/Interviewbit.Tests/TimeComplexity/TimeComplexity.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Interviewbit.Problems.TimeComplexity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Interviewbit.Problems.Tests.TimeComplexity
+{
+    public class TimeComplexity
+    {
+        [Fact]
+        public void FindMinPathSolutionTest()
+        {
+            var solution = new FindMinPathSolution();
+
+            var vector = new List<List<int>>() { new List<int> { 1, 2, 3 }, new List<int> { 1, 2, 3 }, new List<int> { 1, 2, 3 } };
+            solution.CallsCount = 0;
+            solution.FindMinPath(vector, 0, 0);
+            Assert.Equal(17, solution.CallsCount);
+            Assert.Equal(16, solution.CallsCount2);
+        }
+    }
+}
