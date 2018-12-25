4f283d2c4edd2ab379f9fb73a4587c83f1195a0b
Orest Shpak <o.shpak92@gmail.com>
2018-12-26 00:03:31 +0200

    905. Sort Array By Parity

---
+++ LeetCodeProblems/LeetCode.Problems.Tests/Arrays/Easy/ArraysTests.Easy.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Problems.Arrays.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Problems.Tests.Arrays.Easy
+{
+    public partial class ArraysTests
+    {
+        [Fact]
+        public void SortArrayByParityTest()
+        {
+            var solution = new SortArrayByParityProblem();
+            Assert.Equal(new int[] { 4, 2, 1, 3 }, solution.SortArrayByParity(new int[] { 3, 1, 2, 4 }));
+            Assert.Equal(new int[] { 4, 2, 1, 3 }, solution.SortArrayByParity(new int[] { 4, 1, 2, 3 }));
+            Assert.Equal(new int[] { 1, 3, 5, 7 }, solution.SortArrayByParity(new int[] { 1, 3, 5, 7 }));
+            Assert.Equal(new int[] { 2, 4, 6, 8 }, solution.SortArrayByParity(new int[] { 2, 4, 6, 8 }));
+            Assert.Equal(new int[] { 0, 1, 3 }, solution.SortArrayByParity(new int[] { 0, 1, 3 }));
+            Assert.Equal(new int[] { 1, 3, 5 }, solution.SortArrayByParity(new int[] { 1, 3, 5 }));
+        }
+    }
+}
---
+++ LeetCodeProblems/LeetCode.Problems/Arrays/Easy/SortArrayByParityProblem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Arrays.Easy
+{
+    /// <summary>
+    /// 905. Sort Array By Parity
+    /// https://leetcode.com/problems/sort-array-by-parity/
+    /// </summary>
+    public class SortArrayByParityProblem
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            if (A == null) throw new ArgumentNullException(nameof(A));
+            var last = A.Length - 1;
+
+            for (int i = 0; i < last; i++)
+            {
+                if(A[i] % 2 != 0)
+                {
+                    while (last >= 0 && A[last] % 2 != 0)
+                        last--;
+
+                    if (last <= i)
+                        return A;
+
+                    var tmp = A[last];
+                    A[last] = A[i];
+                    A[i] = tmp;
+                    last--;
+                }
+            }
+
+            return A;
+        }
+    }
+}
