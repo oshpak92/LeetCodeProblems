46befa9befdc78458202709c7776815d269b6923
Orest Shpak <o.shpak92@gmail.com>
2019-01-15 23:12:46 +0200

    268. Missing Number

---
+++ LeetCodeProblems/LeetCode.Problems/Arrays/Easy/MissingNumberProblem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Arrays.Easy
+{
+    /// <summary>
+    /// 268. Missing Number
+    /// https://leetcode.com/problems/missing-number/
+    /// Runtime: 128 ms, faster than 98.08% of C# online submissions for Missing Number.
+    /// </summary>
+    public class MissingNumberProblem
+    {
+        public int MissingNumber(int[] nums)
+        {
+            if (nums.Length == 0)
+                throw new ArgumentException(nameof(nums));
+
+            var sum = 0;
+            foreach (var num in nums)
+            {
+                sum += num;
+            }
+            var expectedSum = (double)nums.Length / 2 * (nums.Length + 1);
+            return (int)expectedSum - sum;
+        }
+    }
+}
