55ca5afeda821b090739c9ccf78678f570cd9e05
Orest Shpak <o.shpak92@gmail.com>
2019-02-11 23:41:34 +0200

    28. Implement strStr()

--- LeetCodeProblems/LeetCode.Problems.Tests/Strings/StringsTests.Easy.cs
+++ LeetCodeProblems/LeetCode.Problems.Tests/Strings/StringsTests.Easy.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCode.Problems.Strings;
+using LeetCode.Problems.Strings.Easy;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -35,6 +36,15 @@ namespace LeetCode.Problems.Tests.Strings
             var solution = new FirstUniqueCharacterinaStringProblem();
             Assert.Equal(0, solution.FirstUniqChar("leetcode"));
             Assert.Equal(2, solution.FirstUniqChar("loveleetcode"));
+        }
+
+        [Fact]
+        public void ImplementstrStrProblemTest()
+        {
+            var solution = new ImplementstrStrProblem();
+            Assert.Equal(2, solution.StrStr("hello", "ll"));
+            Assert.Equal(-1, solution.StrStr("aaaaa", "bba"));
+            Assert.Equal(0, solution.StrStr("mississippi", "mississippi"));
         }
     }
 }
---
+++ LeetCodeProblems/LeetCode.Problems/Strings/Easy/ImplementstrStrProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Strings.Easy
+{
+    /// <summary>
+    /// 28. Implement strStr()
+    /// https://leetcode.com/problems/implement-strstr/
+    /// Runtime: 1876 ms, faster than 2.94% of C# online submissions for Implement strStr().
+    /// Memory Usage: 20.1 MB, less than 0.84% of C# online submissions for Implement strStr().
+    /// </summary>
+    public class ImplementstrStrProblem
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle))
+                return 0;
+            
+            if (string.IsNullOrEmpty(haystack) || string.IsNullOrEmpty(needle) || haystack.Length < needle.Length)
+                return -1;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if(haystack[i] == needle[0])
+                {
+                    var _i = i++;
+                    var j = 1;
+                    for (; j < needle.Length && i < haystack.Length; j++, i++)
+                    {
+                        if (haystack[i] != needle[j])
+                            break;
+                    }
+                    if (j == needle.Length)
+                        return _i;
+                    else
+                        i = _i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
