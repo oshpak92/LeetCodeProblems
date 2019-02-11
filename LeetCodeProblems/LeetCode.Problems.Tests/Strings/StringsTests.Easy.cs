5abe86ef4a15a08222885b959f225b2d8dd7bb1f
Orest Shpak <o.shpak92@gmail.com>
2019-02-11 22:09:11 +0200

    344. Reverse String

---
+++ LeetCodeProblems/LeetCode.Problems.Tests/Strings/StringsTests.Easy.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Problems.Strings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Problems.Tests.Strings
+{
+    public partial class StringsTests
+    {
+        [Fact]
+        public void ReverseStringProblemTest()
+        {
+            var solution = new ReverseStringProblem();
+            var input = new char[] { 'h', 'e', 'l', 'l', 'o' };
+            solution.ReverseString(input);
+            Assert.Equal(new char[] { 'o', 'l', 'l', 'e', 'h' }, input);
+
+            input = new char[] { 'h' };
+            solution.ReverseString(input);
+            Assert.Equal(new char[] { 'h' }, input);
+
+            input = new char[] {  };
+            solution.ReverseString(input);
+            Assert.Equal(new char[] {  }, input);
+
+            input = new char[] { 'h', 'e' };
+            solution.ReverseString(input);
+            Assert.Equal(new char[] { 'e', 'h' }, input);
+        }
+    }
+}
---
+++ LeetCodeProblems/LeetCode.Problems/Strings/ReverseStringProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Strings
+{
+    /// <summary>
+    /// 344. Reverse String
+    /// https://leetcode.com/problems/reverse-string/
+    /// Runtime: 440 ms, faster than 33.62% of C# online submissions for Reverse String.
+    /// Memory Usage: 21.5 MB, less than 0.97% of C# online submissions for Reverse String.
+    /// </summary>
+    public class ReverseStringProblem
+    {
+        public void ReverseString(char[] s)
+        {
+            if (s.Length <= 1)
+                return;
+
+            var length = s.Length;
+            var i = 0;
+            while (i <= length - 1 - i)
+            {
+                var tmp = s[i];
+                s[i] = s[length - 1 - i];
+                s[length - 1 - i] = tmp;
+                i++;
+            }
+        }
+    }
+}
