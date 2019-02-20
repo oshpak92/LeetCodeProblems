05f93d5551ce53a90332f93fd27ebdba4447f437
Orest Shpak <o.shpak92@gmail.com>
2019-02-20 22:25:19 +0200

    22. Generate Parentheses

---
+++ LeetCodeProblems/LeetCode.Problems.Tests/Strings/StringsTests.Medium.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Problems.Strings.Easy;
+using LeetCode.Problems.Strings.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Problems.Tests.Strings
+{
+    public partial class StringsTests
+    {
+        [Fact]
+        public void GenerateParenthesesProblemTest()
+        {
+            var solution = new GenerateParenthesesProblem();
+            var result = solution.GenerateParenthesis(3);
+            Assert.Equal(5, result.Count());
+
+            var braketsValidator = new ValidParenthesesProblem();
+            foreach(var item in result)
+            {
+                Assert.True(braketsValidator.IsValid(item));
+            }
+        }
+    }
+}
---
+++ LeetCodeProblems/LeetCode.Problems/Strings/Medium/GenerateParenthesesProblem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Strings.Medium
+{
+    /// <summary>
+    /// 22. Generate Parentheses
+    /// https://leetcode.com/problems/generate-parentheses/
+    /// Runtime: 284 ms, faster than 90.06% of C# online submissions for Generate Parentheses.
+    /// Memory Usage: 31.3 MB, less than 16.39% of C# online submissions for Generate Parentheses.
+    /// Solution from leetcode
+    /// </summary>
+    public class GenerateParenthesesProblem
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var result = new List<string>();
+            backtrack(result, "", 0, 0, n);
+            return result;
+        }
+
+        private void backtrack(IList<string> result, string current, int close, int open, int max)
+        {
+            if(current.Length == max * 2)
+            {
+                result.Add(current);
+                return;
+            }
+
+            if (open < max)
+                backtrack(result, current + "(", close, open + 1, max);
+            if (close < open)
+                backtrack(result, current + ")", close + 1, open, max);
+        }
+    }
+}
