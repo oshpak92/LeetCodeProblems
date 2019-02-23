e8da8044e7cda4f23bb774384a3fb3645e81673d
Orest Shpak <o.shpak92@gmail.com>
2019-02-23 21:31:23 +0200

    3. Longest Substring Without Repeating Characters

--- LeetCodeProblems/LeetCode.Problems.Tests/Strings/StringsTests.Medium.cs
+++ LeetCodeProblems/LeetCode.Problems.Tests/Strings/StringsTests.Medium.cs
@@ -47,5 +47,19 @@ namespace LeetCode.Problems.Tests.Strings
                 Debug.WriteLine(result);
             }
         }
+
+        [Fact]
+        public void LongestSubstringWithoutRepeatingCharactersProblemTest()
+        {
+            var solution = new LongestSubstringWithoutRepeatingCharactersProblem();
+            var result = solution.LengthOfLongestSubstring("abcabcbb");
+            Assert.Equal(3, result);
+            Assert.Equal(1, solution.LengthOfLongestSubstring("bbbbb"));
+            Assert.Equal(3, solution.LengthOfLongestSubstring("pwwkew"));
+            Assert.Equal(1, solution.LengthOfLongestSubstring("b"));
+            Assert.Equal(0, solution.LengthOfLongestSubstring(""));
+            Assert.Equal(2, solution.LengthOfLongestSubstring("au"));
+            Assert.Equal(5, solution.LengthOfLongestSubstring("tmmzuxt"));
+        }
     }
 }
---
+++ LeetCodeProblems/LeetCode.Problems/Strings/Medium/LongestSubstringWithoutRepeatingCharactersProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Strings.Medium
+{
+    /// <summary>
+    /// 3. Longest Substring Without Repeating Characters
+    /// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    /// 
+    /// </summary>
+    public class LongestSubstringWithoutRepeatingCharactersProblem
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            var lettersMap = new Dictionary<char, int>();
+            var maxLength = 0;
+            var pointer = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var nextLength = i + 1 - pointer;
+                if (lettersMap.TryAdd(s[i], i))
+                {
+                    maxLength = System.Math.Max(maxLength, nextLength);
+                }
+                else
+                {
+                    if (pointer > lettersMap[s[i]])
+                        maxLength = System.Math.Max(maxLength, nextLength);
+                    else
+                        pointer = lettersMap[s[i]] + 1;
+
+                    lettersMap[s[i]] = i;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
