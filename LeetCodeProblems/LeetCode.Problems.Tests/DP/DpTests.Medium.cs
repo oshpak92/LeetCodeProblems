8497cdabb97d980f2ff9feebc56a0761bc9a411c
Orest Shpak <o.shpak92@gmail.com>
2019-04-08 00:09:09 +0300

    152. Maximum Product Subarray

--- LeetCodeProblems/LeetCode.Problems.Tests/DP/DpTests.Medium.cs
+++ LeetCodeProblems/LeetCode.Problems.Tests/DP/DpTests.Medium.cs
@@ -15,5 +15,23 @@ namespace LeetCode.Problems.Tests.DP
 
             Assert.Equal(5, solution.LongestSubstring("ababbc", 2));
         }
+
+        [Fact]
+        public void MaximumProductSubarrayProblemTest()
+        {
+            var solution = new MaximumProductSubarrayProblem();
+
+            Assert.Equal(6, solution.MaxProduct(new int[] { 2, 3, -2, 4 }));
+            Assert.Equal(0, solution.MaxProduct(new int[] { -2, 0, -1 }));
+            Assert.Equal(24, solution.MaxProduct(new int[] { -2, 3, -4 }));
+            Assert.Equal(4, solution.MaxProduct(new int[] { 3, -1, 4 }));
+            Assert.Equal(16, solution.MaxProduct(new int[] { 3, -1, 4, 4 }));
+            Assert.Equal(12, solution.MaxProduct(new int[] { -4, -3 }));
+            Assert.Equal(24, solution.MaxProduct(new int[] { 2, -5, -2, -4, 3 }));
+            Assert.Equal(432, solution.MaxProduct(new int[] { 1, -2, 3, -4, -3, -4, -3 }));
+            Assert.Equal(6, solution.MaxProduct(new int[] { -1, -2, -3, 0 }));
+            Assert.Equal(27, solution.MaxProduct(new int[] { 0, -3, 1, 3, -3, -1, 2 }));
+            Assert.Equal(2400, solution.MaxProduct(new int[] {1, -2, 0, 1, -4, 1, 1, 5, 4, -1, 6, 4, 1, -5, 0, -1, -5, 1, -6, -4}));
+        }
     }
 }
---
+++ LeetCodeProblems/LeetCode.Problems/DP/Medium/MaximumProductSubarrayProblem.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.DP.Medium
+{
+    /// <summary>
+    /// 152. Maximum Product Subarray
+    /// https://leetcode.com/problems/maximum-product-subarray/
+    /// </summary>
+    public class MaximumProductSubarrayProblem
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int prod = 1;
+            int result = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                prod = prod * nums[i];
+                result = System.Math.Max(prod, result);
+                if (prod == 0)
+                {
+                    prod = 1;
+                }
+            }
+            prod = 1;
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+
+                prod = prod * nums[i];
+                result = System.Math.Max(prod, result);
+                if (prod == 0)
+                {
+                    prod = 1;
+                }
+            }
+            return result;
+        }
+        //public int MaxProduct(int[] nums)
+        //{
+        //    var nums1 = new int[nums.Length];
+        //    Array.Copy(nums, nums1, nums.Length);
+
+        //    var max = nums[0];
+        //    var max2 = nums[nums.Length - 1];
+
+        //    int? lessZeroProduct = nums[0] < 0 ? (int?)nums[0] : null;
+        //    nums1[0] = nums[0] == 0 ? 1 : nums[0];
+        //    for (int i = 1; i < nums1.Length; i++)
+        //    {
+        //        var tmp = nums1[i - 1] < 0 ? 1: nums1[i - 1];
+        //        if (nums1[i] == 0)
+        //            lessZeroProduct = null;
+
+        //        var product = nums1[i] * tmp;
+        //        if(product < 0 && lessZeroProduct.HasValue)
+        //        {
+        //            product = lessZeroProduct.Value * product;
+        //            lessZeroProduct = null;
+        //        }
+        //        else if (product < 0 && !lessZeroProduct.HasValue)
+        //        {
+        //            lessZeroProduct = product;
+        //        }
+        //        if (product > nums1[i])
+        //        {
+        //            nums1[i] = product;
+        //        }
+        //        max = System.Math.Max(max, nums1[i]);
+        //    }
+        //    lessZeroProduct = nums[nums.Length-1] < 0 ? (int?)nums[nums.Length - 1] : null;
+        //    nums[nums.Length - 1] = nums[nums.Length - 1] == 0 ? 1 : nums[nums.Length - 1];
+        //    for (int i = nums.Length - 2 ; i >= 0; i--)
+        //    {
+        //        var tmp = nums[i + 1] < 0 ? 1 : nums[i + 1];
+        //        tmp = tmp == 0 ? 1 : tmp;
+        //        if (nums[i] == 0)
+        //        {
+        //            lessZeroProduct = null;
+        //        }
+
+        //        var product = nums[i] * tmp;
+        //        if (product < 0 && lessZeroProduct.HasValue)
+        //        {
+        //            product = lessZeroProduct.Value * product;
+        //            lessZeroProduct = null;
+        //        }
+        //        else if (product < 0 && !lessZeroProduct.HasValue)
+        //        {
+        //            lessZeroProduct = product;
+        //        }
+        //        if (product > nums[i])
+        //        {
+        //            nums[i] = product;
+        //        }
+        //        max2 = System.Math.Max(max2, nums[i]);
+        //    }
+
+        //    return System.Math.Max( max, max2);
+        //}
+    }
+}
