c92826aff832b0ad4de5f5e08367f81397318e07
Orest Shpak <o.shpak92@gmail.com>
2019-04-10 23:13:45 +0300

    198. House Robber

--- LeetCodeProblems/LeetCode.Problems.Tests/DP/DpTests.Easy.cs
+++ LeetCodeProblems/LeetCode.Problems.Tests/DP/DpTests.Easy.cs
@@ -21,5 +21,16 @@ namespace LeetCode.Problems.Tests.DP
             var solution = new MaximumProductofThreeNumbers();
             Assert.Equal(24, solution.MaximumProduct(new int[] { 1, 2, -3, -4 }));
         }
+
+        [Fact]
+        public void HouseRobberProblemTest()
+        {
+            var solution = new HouseRobberProblem();
+            Assert.Equal(4, solution.Rob(new int[] { 1, 2, 3, 1}));
+            Assert.Equal(1, solution.Rob(new int[] { 1 }));
+            Assert.Equal(2, solution.Rob(new int[] { 1, 2 }));
+            Assert.Equal(0, solution.Rob(new int[] { }));
+            Assert.Equal(4, solution.Rob(new int[] { 2, 1, 1, 2 }));
+        }
     }
 }
---
+++ LeetCodeProblems/LeetCode.Problems/DP/Easy/HouseRobberProblem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.DP.Easy
+{
+    /// <summary>
+    /// 198. House Robber
+    /// https://leetcode.com/problems/house-robber/submissions/
+    /// </summary>
+    public class HouseRobberProblem
+    {
+        //public int Rob(int[] nums)
+        //{
+        //    if (nums.Length == 1)
+        //        return nums[0];
+        //    else if (nums.Length == 0)
+        //        return 0;
+
+        //    var max = System.Math.Max(nums[0], nums[1]);
+        //    for (int i = 2; i < nums.Length; i++)
+        //    {
+        //        var curentMax = nums[i - 2] + nums[i];
+        //        if (curentMax >= max)
+        //            nums[i] = curentMax;
+        //        else
+        //            nums[i] = max - nums[i];
+
+        //        max = System.Math.Max(max, nums[i]);
+        //    }
+
+        //    return max;
+        //}
+
+        public int Rob(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+            else if (nums.Length == 0)
+                return 0;
+
+            var memo = new int[nums.Length];
+            for (int i = 0; i < memo.Length; i++)
+                memo[i] = -1;
+            
+
+            return rob(nums, memo, nums.Length - 1);
+        }
+
+        private int rob(int[] nums, int[] memo, int i)
+        {
+            if (i < 0)
+            {
+                return 0;
+            }
+            if (memo[i] >= 0)
+                return memo[i];
+            
+            var max = System.Math.Max(rob(nums, memo, i - 2) + nums[i], rob(nums, memo, i - 1));
+            memo[i] = max;
+            return max;
+        }
+    }
+}
