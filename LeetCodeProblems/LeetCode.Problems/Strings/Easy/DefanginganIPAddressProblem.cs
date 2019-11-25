88d2dd2bd2769e3b153b6f5435248f7e5070d6ac
Orest Shpak <o.shpak92@gmail.com>
2019-11-25 20:38:37 +0200

    1108. Defanging an IP Address

---
+++ LeetCodeProblems/LeetCode.Problems/Strings/Easy/DefanginganIPAddressProblem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Problems.Strings.Easy
+{
+	public class DefanginganIPAddressProblem
+	{
+		public string DefangIPaddr(string address)
+		{
+			var result = new StringBuilder();
+			foreach (var character in address)
+			{
+				if (character == '.')
+					result.Append("[.]");
+				else
+					result.Append(character);
+
+			}
+			return result.ToString();
+		}
+
+		//public string DefangIPaddr(string address)
+		//{
+		//	string pattern = "[.]";
+		//	string replacement = "[.]";
+
+		//	var regEx = new Regex(pattern);
+		//	var result = regEx.Replace(address, replacement);// = Regex.Replace(regEx.Replace(address, replacement), @"\s+", " ");
+		//	return result;
+		//}
+	}
+}
