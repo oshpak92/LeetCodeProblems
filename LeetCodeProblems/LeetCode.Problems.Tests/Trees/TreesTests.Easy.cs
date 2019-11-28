876386dc8363c6abc708c13fe416a4b54d2d3614
Orest Shpak <o.shpak92@gmail.com>
2019-11-28 19:02:15 +0200

    104. Maximum Depth of Binary Tree

---
+++ LeetCodeProblems/LeetCode.Problems.Tests/Trees/TreesTests.Easy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Problems.Arrays.Medium;
+using LeetCode.Problems.Trees.Easy;
+using Xunit;
+
+namespace LeetCode.Problems.Tests.Trees
+{
+	public partial class TreesTests
+	{
+		[Fact]
+		public void MaximumDepthofBinaryTreeProblemTest()
+		{
+			var solution = new MaximumDepthofBinaryTreeProblem();
+			var tree = new TreeNode(3)
+			{
+				left = new TreeNode(9),
+				right = new TreeNode(20)
+				{
+					left = new TreeNode(15),
+					right = new TreeNode(7)
+				}
+			};
+			Assert.Equal(3, solution.MaxDepth(tree));
+		}
+	}
+}
---
+++ LeetCodeProblems/LeetCode.Problems/Trees/Easy/MaximumDepthofBinaryTreeProblem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Problems.Arrays.Medium;
+
+namespace LeetCode.Problems.Trees.Easy
+{
+	public class MaximumDepthofBinaryTreeProblem
+	{
+		public int MaxDepth(TreeNode root)
+		{
+			if (root == null)
+				return 0;
+
+			return 1 + System.Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+		}
+	}
+}
