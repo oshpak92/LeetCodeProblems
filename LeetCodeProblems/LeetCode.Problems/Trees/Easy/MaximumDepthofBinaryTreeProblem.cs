90ebacbc5e9115921d92587c20805f3dd7683632
Orest Shpak <o.shpak92@gmail.com>
2019-11-28 19:03:02 +0200

    code refactoring

--- LeetCodeProblems/LeetCode.Problems/Trees/Easy/MaximumDepthofBinaryTreeProblem.cs
+++ LeetCodeProblems/LeetCode.Problems/Trees/Easy/MaximumDepthofBinaryTreeProblem.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using LeetCode.Problems.Arrays.Medium;
+﻿using LeetCode.Problems.Arrays.Medium;
 
 namespace LeetCode.Problems.Trees.Easy
 {
