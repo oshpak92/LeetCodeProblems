1d322593f6f6192a91b33c05aa806f53c61cacf3
Orest Shpak <o.shpak92@gmail.com>
2020-01-27 21:27:47 +0200

    242. Valid Anagram

---
+++ LeetCodeProblems/LeetCode.Problems/HashTable/Easy/ValidAnagramProblem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.HashTable.Easy
+{
+	public class ValidAnagramProblem
+	{
+		public bool IsAnagram(string s, string t)
+		{
+			if (s.Length != t.Length)
+				return false;
+
+			var bufer1 = new Dictionary<char, int>();
+			foreach (var ch in s)
+			{
+				if (bufer1.ContainsKey(ch))
+					bufer1[ch]++;
+				else
+					bufer1[ch] = 1;
+			}
+
+			var bufer2 = new Dictionary<char, int>();
+			foreach (var ch in t)
+			{
+				if (bufer2.ContainsKey(ch))
+					bufer2[ch]++;
+				else
+					bufer2[ch] = 1;
+			}
+
+			foreach (var keyVal in bufer1)
+			{
+				if (!bufer2.ContainsKey(keyVal.Key) || bufer2[keyVal.Key] != keyVal.Value)
+					return false;
+			}
+
+			return true;
+		}
+	}
+}
